6f504af81b972e6a7975b16259169ac6be00fa46
Michael Yan <rain@rainboyan.com>
2026-09-04 23:25:14 +0800

    Generated by Spring Boot 2.7.18

---
+++ src/main/groovy/grace/guides/GraceGuideApplication.groovy
@@ -0,0 +1,13 @@
+package grace.guides
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GraceGuideApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(GraceGuideApplication, args)
+	}
+
+}
